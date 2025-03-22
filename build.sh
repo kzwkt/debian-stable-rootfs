10f29fd8495898a029861004fe18ac220b640897
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-22 09:55:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y debootstrap
 codename="oracular"
-echo $codenae > release
+echo $codenae > version
 sudo debootstrap --variant=minbase --arch=amd64 $codename rootfs http://archive.ubuntu.com/ubuntu
 sudo tar -czf rootfs.tar.gz -C rootfs .
 
