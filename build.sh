7f9ca6a2e27f3e89bc1d35d2248e68565efab6f1
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-04-17 10:48:19 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y debootstrap
-codename="oracular"
+codename="noble"
 sudo debootstrap --variant=minbase --arch=amd64 $codename rootfs http://archive.ubuntu.com/ubuntu
 sudo rm -rf rootfs/var/cache/apt
 sudo tar -czf rootfs.tar.gz -C rootfs .
